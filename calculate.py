ca87a90e19bfe540333e73d193069ca6d817415e
Karen Lauwers <karen.lauwers@student.ucll.be>
2023-12-26 18:57:39 +0100

    No changes. Updated in other file calculate_updateKa

--- calculate.py
+++ calculate.py
@@ -20,16 +20,16 @@ for p in players:
 for i in range(2):
     dealer.add_card(deck.deal())
 
-print(player1.cards)
-print(player2.cards)
-print(dealer.cards)
+print('player1', player1.cards)
+print('player2', player2.cards)
+print('dealer', dealer.cards)
 
 # Deal another card to both players
 for p in players:
     p.add_card(deck.deal())
 
-print(player1.cards)
-print(player2.cards)
+print('player1',player1.cards)
+print('player2', player2.cards)
 
 # Calculate the total value of hands for both players and dealer
 for p in players:
